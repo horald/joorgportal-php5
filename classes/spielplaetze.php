199c0f8d4b0b9577c540f702d1375a13b9718ae3
Horst Meyer <horald@horald-laptop.(none)>
2014-07-22 07:21:45 +0200

    Spielplatz included

--- classes/spielplaetze.php
+++ classes/spielplaetze.php
@@ -2,7 +2,6 @@
 echo "Spielpl&aumltze in K&oumlln<br>";
 ?>
 
-<!DOCTYPE html >
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
--- git.sh
+++ git.sh
@@ -1,8 +1,9 @@
 git add --all
-let uebergabe=$1
-if [ "$uebergabe" != "" ]; then
-  git commit -m "$uebergabe"
+if [ "$1" != "" ]; then
+  echo "$1"
+  git commit -m "$1"
 else
+  echo "Undocumented Changes"
   git commit -m "Undocumented Changes"
 fi
 git push
